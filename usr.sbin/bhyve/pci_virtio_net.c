Happy-Ferret/unix-history-repo

commit 1cb0291517c12dedbcc9c265ce58197df6941f4b

Simplify virtio ring num-available calculation.
Submitted by:	Chris Torek, torek at torek dot net


git-svn-id: svn+ssh://svn.freebsd.org/base/head@247871 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.sbin/bhyve/pci_virtio_net.c
@@ -172,12 +172,17 @@ hq_num_avail(struct vring_hqueue *hq)
 {
 	int ndesc;
 
-	if (*hq->hq_avail_idx >= hq->hq_cur_aidx)
-		ndesc = *hq->hq_avail_idx - hq->hq_cur_aidx;
-	else
-		ndesc = UINT16_MAX - hq->hq_cur_aidx + *hq->hq_avail_idx + 1;
+	/*
+	 * We're just computing (a-b) in GF(216).
+	 *
+	 * The only glitch here is that in standard C,
+	 * uint16_t promotes to (signed) int when int has
+	 * more than 16 bits (pretty much always now), so
+	 * we have to force it back to unsigned.
+	 */
+	ndesc = (unsigned)*hq->hq_avail_idx - (unsigned)hq->hq_cur_aidx;
 
-	assert(ndesc >= 0 && ndesc <= hq->hq_size);
+	assert(ndesc <= hq->hq_size);
 
 	return (ndesc);
 }

usr.sbin/bhyve/pci_virtio_block.c
@@ -164,14 +164,19 @@ pci_vtblk_iosize(struct pci_devinst *pi)
 static int
 hq_num_avail(struct vring_hqueue *hq)
 {
-	int ndesc;
+	uint16_t ndesc;
 
-	if (*hq->hq_avail_idx >= hq->hq_cur_aidx)
-		ndesc = *hq->hq_avail_idx - hq->hq_cur_aidx;
-	else
-		ndesc = UINT16_MAX - hq->hq_cur_aidx + *hq->hq_avail_idx + 1;
+	/*
+	 * We're just computing (a-b) in GF(216).
+	 *
+	 * The only glitch here is that in standard C,
+	 * uint16_t promotes to (signed) int when int has
+	 * more than 16 bits (pretty much always now), so
+	 * we have to force it back to unsigned.
+	 */
+	ndesc = (unsigned)*hq->hq_avail_idx - (unsigned)hq->hq_cur_aidx;
 
-	assert(ndesc >= 0 && ndesc <= hq->hq_size);
+	assert(ndesc <= hq->hq_size);
 
 	return (ndesc);
 }

commit 0144f3a8ec95308844f5d2845a97953b50b000d8

Change the type of 'ndesc' from 'int' to 'uint16_t' so that descriptor index wraparound is handled correctly.
The gory details are available here:
http://lists.freebsd.org/pipermail/freebsd-virtualization/2013-March/001119.html

This fixes a regression introduced in r247871.

Pointed out by:	Bruce Evans, Chris Torek

usr.sbin/bhyve/pci_virtio_net.c
@@ -170,7 +170,7 @@ pci_vtnet_iosize(struct pci_devinst *pi)
 static int
 hq_num_avail(struct vring_hqueue *hq)
 {
-	int ndesc;
+	uint16_t ndesc;
 
 	/*
 	 * We're just computing (a-b) in GF(216).
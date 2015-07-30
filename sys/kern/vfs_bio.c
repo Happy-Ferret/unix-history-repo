Happy-Ferret/unix-history-repo

commit 1464879601f07e8da205604199ea3552e520c091

vfs: fix off-by-one error in vfs_buf_check_mapped
The check added in r285872 can trigger for valid buffers if the buffer space
used happens to be just after unmapped_buf in KVA space.

Discussed with: kib
Sponsored by: Citrix Systems R&D


git-svn-id: svn+ssh://svn.freebsd.org/base/head@286076 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/kern/vfs_bio.c
@@ -955,7 +955,7 @@ vfs_buf_check_mapped(struct buf *bp)
 	    ("mapped buf: b_kvabase was not updated %p", bp));
 	KASSERT(bp->b_data != unmapped_buf,
 	    ("mapped buf: b_data was not updated %p", bp));
-	KASSERT(bp->b_data < unmapped_buf || bp->b_data > unmapped_buf +
+	KASSERT(bp->b_data < unmapped_buf || bp->b_data => unmapped_buf +
 	    MAXPHYS, ("b_data + b_offset unmapped %p", bp));
 }
 

commit 04bae68389c6838c0b56480e0d342c6d32d265fa

vfs: fill fallout from r286076
This right operator is >= not =>.

Reported by: cem

sys/kern/vfs_bio.c
@@ -955,7 +955,7 @@ vfs_buf_check_mapped(struct buf *bp)
 	    ("mapped buf: b_kvabase was not updated %p", bp));
 	KASSERT(bp->b_data != unmapped_buf,
 	    ("mapped buf: b_data was not updated %p", bp));
-	KASSERT(bp->b_data < unmapped_buf || bp->b_data => unmapped_buf +
+	KASSERT(bp->b_data < unmapped_buf || bp->b_data >= unmapped_buf +
 	    MAXPHYS, ("b_data + b_offset unmapped %p", bp));
 }
 
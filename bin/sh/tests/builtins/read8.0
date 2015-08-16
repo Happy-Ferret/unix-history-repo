Happy-Ferret/unix-history-repo

commit 0c34f7211093f63eb96624c21b6bcdae81e1a241

sh: Fix 'read' if all chars before the first IFS char are backslash-escaped.
Backslash-escaped characters did not set the flag for a non-IFS character.

MFC after:	2 weeks


git-svn-id: svn+ssh://svn.freebsd.org/base/head@212339 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

bin/sh/miscbltin.c
@@ -174,6 +174,7 @@ readcmd(int argc __unused, char **argv __unused)
 			continue;
 		if (backslash) {
 			backslash = 0;
+			startword = 0;
 			if (c != '\n')
 				STPUTC(c, p);
 			continue;

tools/regression/bin/sh/builtins/read4.0
@@ -1,0 +1,10 @@
+# $FreeBSD$
+
+printf '%s\n' '\a\ b c'	| { read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\a b\ c'	| { read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\a\:b:c'	| { IFS=: read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\a:b\:c'	| { IFS=: read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\\ a'	| { read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\\:a'	| { IFS=: read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\\\ a'	| { read a b; printf '%s\n' "x${a}x${b}x"; }
+printf '%s\n' '\\\:a'	| { IFS=: read a b; printf '%s\n' "x${a}x${b}x"; }

tools/regression/bin/sh/builtins/read4.0.stdout
@@ -1,0 +1,8 @@
+xa bxcx
+xaxb cx
+xa:bxcx
+xaxb:cx
+x\xax
+x\xax
+x\ axx
+x\:axx

commit 0ae7d417a17391e4b9dcddfa4ba81ba1097e466f

sh: Backslash-newline should not affect field splitting in read builtin.
This was originally broken in r212339 in 2010.

bin/sh/miscbltin.c
@@ -191,9 +191,10 @@ readcmd(int argc __unused, char **argv __unused)
 		CHECKSTRSPACE(1, p);
 		if (backslash) {
 			backslash = 0;
-			startword = 0;
-			if (c != '\n')
+			if (c != '\n') {
+				startword = 0;
 				USTPUTC(c, p);
+			}
 			continue;
 		}
 		if (!rflag && c == '\\') {

bin/sh/tests/builtins/Makefile
@@ -121,6 +121,7 @@ FILES+=		read4.0 read4.0.stdout
 FILES+=		read5.0
 FILES+=		read6.0
 FILES+=		read7.0
+FILES+=		read8.0
 FILES+=		return1.0
 FILES+=		return2.1
 FILES+=		return3.1

bin/sh/tests/builtins/read8.0
@@ -1,0 +1,17 @@
+# $FreeBSD$
+
+read a b c <<\EOF
+\
+A\
+ \
+ \
+ \
+B\
+ \
+ \
+C\
+ \
+ \
+ \
+EOF
+[ "$a.$b.$c" = "A.B.C" ]